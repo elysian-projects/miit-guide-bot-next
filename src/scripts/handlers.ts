cab80c44e7b7019d16a9ce02ae3caad56096c756
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-12-09 00:08:53 +0300

    chore: add main menu test logic

--- src/scripts/handlers.ts
+++ src/scripts/handlers.ts
@@ -1,11 +1,15 @@
+import * as replies from "@/constants/replies";
+import { locations } from "@/env";
 import { MessageHandler } from "@/types/handlers";
+import { createInlineKeyboardMarkup } from "@/utils/buttons";
 
-const start: MessageHandler<string[]> = (ctx, payload) => {
-  if(!payload) {
-    throw new Error("No payload at `start` given!");
-  }
+const start: MessageHandler = (ctx) => {
+  const locationImages = Object.values(locations);
 
-  ctx.sendMessage("Welcome!");
+  const markup = createInlineKeyboardMarkup(locationImages);
+
+  ctx.sendPhoto("https://rut-miit.ru/content/opengraph-image_1_1920x1280.jpg?id_wm=884159");
+  ctx.sendMessage(replies.GREETINGS, {reply_markup: markup});
 };
 
 const help: MessageHandler = (ctx) => {
