e13915fea14db485713e0dee62edc6ff63ebb2fe
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-12-09 00:08:02 +0300

    chore: refactoring

--- src/constants/locations.ts
+++ src/constants/locations.ts
@@ -1,4 +1,15 @@
 import { locations } from "@/env";
+import { LocationType } from "@/types/data";
 
-export const locationValues: string[] = Object.values(locations).map(location => location.value);
-export const locationLabels: string[] = Object.values(locations).map(location => location.label);
+/**
+ * Extracts value with `prop` from the list of locations and returns an array of its values
+ * @private
+ */
+const getLocationProp = (locationsList: LocationType[], prop: keyof LocationType): string[] => {
+  console.log(locationsList);
+
+  return Object.values(locationsList).map(location => location[prop]);
+};
+
+export const locationValues: string[] = getLocationProp(Object.values(locations), "value");
+export const locationLabels: string[] = getLocationProp(Object.values(locations), "label");
