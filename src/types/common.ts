3592dedd800bb2d87b2230cccd2fd3d853748263
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-12-22 11:09:49 +0300

    refactor(controller): replace general interface for state controller

--- src/types/common.ts
+++ src/types/common.ts
@@ -3,3 +3,11 @@ import { UserId, UserState } from "./data";
 export type UpdatePropValue = {
   <P extends keyof UserState, K extends UserState[P]>(userId: UserId, propName: P, propValue: K): void
 }
+
+export interface IStoreController {
+  getUserState: (userId: UserId) => UserState,
+  addUser: (userId: UserId) => void,
+  removeUser: (userId: UserId) => void,
+  updatePropValue: UpdatePropValue,
+  resetUserState: (userId: UserId) => void
+}
