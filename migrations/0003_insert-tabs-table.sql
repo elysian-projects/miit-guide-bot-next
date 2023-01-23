8757ad107f9d2d094329cb5be7e99d01b99c7abf
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-01-23 12:39:24 +0300

    chore(migrations): add insert db data

--- migrations/0003_insert-tabs-table.sql
+++ migrations/0003_insert-tabs-table.sql
@@ -1,1 +1,3 @@
-INSERT INTO tabs (tab_name, tab_value) VALUES ('МИИТ в годы ВОВ', 'ww2');
+INSERT INTO tabs (tab_label, tab_value, tab_type) VALUES ('МИИТ в годы ВОВ', 'ww2', 'article');
+INSERT INTO tabs (tab_label, tab_value, tab_type) VALUES ('Улица', 'street', 'location');
+INSERT INTO tabs (tab_label, tab_value, tab_type) VALUES ('Корпус 1', 'building1', 'location');
