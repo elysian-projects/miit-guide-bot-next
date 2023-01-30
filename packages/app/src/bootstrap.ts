d6ab8c8dc3bdba2031ea110ea2563d1fa305be4b
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-01-30 17:13:54 +0300

    chore(error_handling): add proper error handling messages

--- packages/app/src/bootstrap.ts
+++ packages/app/src/bootstrap.ts
@@ -1,6 +1,8 @@
 import { onHelp, onStart, onUnknown } from "@/chat/commands";
+import { AxiosError } from "axios";
 import { config as dotenvConfig } from "dotenv";
 import { Bot, GrammyError, HttpError } from "grammy";
+import { NO_CONTENT, NO_TG_CONNECTION } from "./chat/constants";
 import { keyboardControls } from "./chat/controls";
 import { KeyboardRouter } from "./controllers/keyboardClickController";
 import { StoreController } from "./controllers/storeController";
@@ -34,13 +36,38 @@ bot.on("message", onUnknown);
 bot.catch(err => {
   const ctx = err.ctx;
   console.error(`Error while handling update ${ctx.update.update_id}:`);
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
-  } else if (e instanceof HttpError) {
-    console.error("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
+  const e = err.error as {description: string};
+
+  switch(e.constructor) {
+    // Appears when there is a problem on the client side and the Telegram server could not process the response
+    case GrammyError: {
+      console.error("Error in request:", e.description);
+      ctx.reply("Произошла ошибка, приносим свои извинения!");
+      break;
+    }
+
+    // Appears when the Telegram servers are not available or failed to connect, this might be due to
+    // loss of internet connection or any other network reasons. The problem is, this cannot be processed properly
+    case HttpError: {
+      console.error("Could not contact Telegram:", e);
+      ctx.reply(NO_TG_CONNECTION);
+      break;
+    }
+
+    // Appear when our server sends a response with status different from `2**` or doesn't respond at all,
+    // in this case the data cannot be fetched or there is just nothing to fetch (empty response with `404` status code)
+    case AxiosError: {
+      console.error("Axios error:", e.description);
+      ctx.reply(NO_CONTENT);
+      break;
+    }
+
+    // Any other kind of error
+    default: {
+      console.error("Unknown error:", e);
+      ctx.reply("Неизвестная ошибка!");
+      break;
+    }
   }
 });
 
