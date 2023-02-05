08dc47ebf05dbf1a7c63310d84993518d2c277bd
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-02-05 13:48:36 +0300

    chore(not_found_page): restyle 404-page

--- packages/client/src/pages/404/404.tsx
+++ packages/client/src/pages/404/404.tsx
@@ -1,11 +1,22 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { NotFoundCode, NotFoundWrapper } from "./404.styles";
 
 export const NotFoundPage: FC = () => {
   return (
-    <>
-      <Typography variant="h2">404!</Typography>
+    <NotFoundWrapper>
+      <NotFoundCode>404</NotFoundCode>
       <Typography variant="h4">Страница не найдена!</Typography>
-    </>
+      <Typography variant="subtitle2" lineHeight={1.3} fontSize={16}>
+        Страница, которую вы ищите, не найдена. Возможно, она была перенесена, удалена или временно недоступна.
+        Проверьте правильность введённого адреса или обратитесь к администратору.
+      </Typography>
+      <Link to="/">
+        <Button variant="contained" color="error">
+          В меню
+        </Button>
+      </Link>
+    </NotFoundWrapper>
   )
 }
