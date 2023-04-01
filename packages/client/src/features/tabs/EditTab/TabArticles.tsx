39a539b011da18904ad3802cd59878dd91280db1
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-04-02 01:25:23 +0300

    chore: add drag-and-drop system in the reorder menu
    * add `react-beautiful-dnd` library
    * make an interactive menu based on drag-and-drop

--- packages/client/src/features/tabs/EditTab/TabArticles.tsx
+++ packages/client/src/features/tabs/EditTab/TabArticles.tsx
@@ -1,11 +1,13 @@
 import { UnfoldMore } from "@mui/icons-material";
-import { Alert, Button, IconButton, Link, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Alert, Button, Link, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { ContentNode, FlatContent } from "common/src";
 import { FC, useEffect, useRef, useState } from "react";
+import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
 import { Separator } from "../../../components/separator";
 import { useHttp } from "../../../hooks/useHttp";
 import { formatDate } from "../../../utils/formatDate";
 import { getAllArticles } from "../../articles/api";
+import { reorderList } from "./scripts";
 
 const MAX_RETRIES = 3;
 
@@ -35,6 +37,11 @@ export const TabArticles: FC<ITabArticlesProps> = ({tabId}) => {
     }
   }, [response]);
 
+  const handleReorder = (ctx: DropResult) => {
+    changedOrder.current = true;
+    setData(reorderList(ctx, data));
+  };
+
   return <>
     {error ? (
       <>
@@ -43,22 +50,38 @@ export const TabArticles: FC<ITabArticlesProps> = ({tabId}) => {
         </Alert>
       </>
     ) : (
-      <>
+      <DragDropContext onDragEnd={handleReorder}>
         <List sx={{ width: "100%"}} disablePadding>
-          {data.map(article => (
-            <ListItem sx={{width: "100%"}} key={article.id}>
-              <ListItemAvatar>
-                <IconButton>
-                  <UnfoldMore />
-                </IconButton>
-              </ListItemAvatar>
-              <ListItemText secondary={article.addedOn ? `Добавлено: ${formatDate(article.addedOn)}` : ""}>
-                <Link href={`/content/articles/edit?id=${article.id}`}>
-                  {article.label}
-                </Link>
-              </ListItemText>
-            </ListItem>
-          ))}
+          <Droppable droppableId="articles-root">
+            {(droppableProvided) => ((
+              <div
+                ref={droppableProvided.innerRef}
+                {...droppableProvided.droppableProps}
+              >
+                {data.map((article, index) => (
+                  <Draggable draggableId={article.id.toString()} index={index} key={article.id}>
+                    {(draggableProvided) => (
+                      <ListItem
+                        sx={{width: "100%"}}
+                        ref={draggableProvided.innerRef}
+                        {...draggableProvided.draggableProps}
+                      >
+                        <ListItemAvatar {...draggableProvided.dragHandleProps}>
+                          <UnfoldMore />
+                        </ListItemAvatar>
+                        <ListItemText secondary={article.addedOn ? `Добавлено: ${formatDate(article.addedOn)}` : ""}>
+                          <Link href={`/content/articles/edit?id=${article.id}`}>
+                            {article.label}
+                          </Link>
+                        </ListItemText>
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+                {droppableProvided.placeholder}
+              </div>
+            ))}
+          </Droppable>
         </List>
         {changedOrder && (
           <>
@@ -68,7 +91,7 @@ export const TabArticles: FC<ITabArticlesProps> = ({tabId}) => {
             </Button>
           </>
         )}
-      </>
+      </DragDropContext>
     )}
   </>;
 };
