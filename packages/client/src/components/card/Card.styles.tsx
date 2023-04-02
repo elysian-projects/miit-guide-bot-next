b05a7b2062b86b707f578279aef7ffa725a8ee14
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-04-02 15:37:08 +0300

    chore: set line view for the tabs

--- packages/client/src/components/card/Card.styles.tsx
+++ packages/client/src/components/card/Card.styles.tsx
@@ -1,17 +1,33 @@
 import styled from "@emotion/styled";
+import { ListView } from "../../types";
 
-export const CardWrapper = styled.div`
+interface ICardWrapperProps {
+  view?: ListView
+}
+
+export const CardWrapper = styled.div<ICardWrapperProps>`
   position: relative;
 
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 1rem;
+  ${props => !props.view || props.view === "grid" ? `
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
 
-  @media(max-width: 800px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
+    @media(max-width: 800px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
 
-  @media(max-width: 530px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
+    @media(max-width: 530px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  ` : `
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+
+    * {
+      display: flex !important;
+      justify-content: space-between;
+    }
+  `}
 `;
---
+++ packages/client/src/types/index.ts
@@ -0,0 +1,1 @@
+export type ListView = "grid" | "line";
