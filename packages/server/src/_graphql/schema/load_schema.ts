10fde74a06f21cc4d83dbea130dff04aa1a39198
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2023-06-05 18:38:52 +0300

    chore: separate entities into different files

--- packages/server/src/_graphql/schema/load_schema.ts
+++ packages/server/src/_graphql/schema/load_schema.ts
@@ -1,8 +1,18 @@
-import { readFileSync } from "fs";
+import fs, { readFileSync, } from "fs";
 import { GraphQLSchema, buildSchema } from "graphql";
 import path from "path";
 
+const BASE_PATH = path.resolve(process.cwd(), "src/_graphql/schema/entities");
+
 export const loadSchema = (): GraphQLSchema => {
-  // TODO: find a better way to write the path to the schema
-  return buildSchema(readFileSync(path.resolve(process.cwd(), "src/_graphql/schema/schema.graphql"), "utf8"));
+  const filesData = loadSchemasPaths().map(file => readFile(path.resolve(BASE_PATH, file)));
+  return buildSchema(filesData.join(""));
+};
+
+const readFile = (filePath: string): string => {
+  return readFileSync(filePath, "utf8");
+};
+
+const loadSchemasPaths = (): string[] => {
+  return fs.readdirSync(BASE_PATH, {encoding: "utf8"});
 };
